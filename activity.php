c6864dbfab8f2289e1e71914c00c6ec33e569899
nikkamaearamburo354-design <nikkamaearamburo354@gmail.com>
2026-02-23 08:45:06 +0800

    Update activity.php

--- activity.php
+++ activity.php
@@ -1,3 +1,43 @@
-<?php 
-echo "Hello, World";
+?php
+$response = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $message = strtolower($_POST["message"]);
+
+    if ($message == "hello") {
+        $response = "Hi there!";
+    } 
+    elseif ($message == "how are you") {
+        $response = "I'm just a simple PHP chatbot ";
+    } 
+    elseif ($message == "bye") {
+        $response = "Goodbye! Have a nice day!";
+    } 
+    else {
+        $response = "Sorry, I don't understand.";
+    }
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP Chatbot</title>
+</head>
+<body>
+
+<h2>Simple PHP Chatbot</h2>
+
+<form method="post">
+    <input type="text" name="message" placeholder="Type a message..." required>
+    <button type="submit">Send</button>
+</form>
+
+<?php
+if ($response != "") {
+    echo "<p><strong>Bot:</strong> $response</p>";
+}
+?>
+
+</body>
+</html>
